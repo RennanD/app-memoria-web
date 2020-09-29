ac681cbd9a043f48f147f59a0b5e37bf47a4eb95
Rennan <rennan.douglas@objetivopi.com.br>
2020-09-29 13:20:19 -0300

    Invite Friends web

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Redirect } from 'react-router-dom';
 
 import Route from './Route';
 import Login from '../pages/Login';
@@ -8,11 +8,21 @@ import Messages from '../pages/Messages';
 import GenericDates from '../pages/GenericDates';
 import AcceptInvites from '../pages/AcceptInvites';
 
+const Accept: React.FC = () => {
+  useEffect(() => {
+    window.location.href =
+      'https://expo.io/--/to-exp/exp%3A%2F%2Fxx-i8s.anonymous.mobile.exp.direct%3A80';
+  }, []);
+
+  return <div />;
+};
+
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Login} />
       <Route path="/accept-invites" exact component={AcceptInvites} />
+      <Route path="/teste" exact component={Accept} />
 
       <Route path="/dashboard" component={Preferences} isPrivate />
       <Route path="/messages" component={Messages} isPrivate />
