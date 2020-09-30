321917e019bf5b549f5fb3596d2e05d40f9514c3
Rennan <rennan.douglas@objetivopi.com.br>
2020-09-30 13:19:02 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://app-memoria.tk',
-  baseURL: 'http://10.0.0.105:3333',
+  baseURL: 'https://app-memoria.tk',
+  // baseURL: 'http://10.0.0.105:3333',
 });
 
 export default api;
