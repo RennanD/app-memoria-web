4a853403df658064e8b5cb1d76daf3b8cfa8a8b8
RennanD <rplaykl@gmail.com>
2020-12-04 20:44:51 -0300

    feat: Create sugestion dates

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://app-memoria.tk',
-  baseURL: 'http://10.0.0.100:3333',
+  baseURL: 'https://api-memoria.herokuapp.com',
+  // baseURL: 'http://10.0.0.100:3333',
 });
 
 export default api;
